84d2140065e7345654e1f0b4b4c2b50e892f9f39
mario_rosales <mariocrosl@gmail.com>
2019-12-31 18:48:55 +0100

    first commit

---
+++ other_demo/src/test/java/com/example/other_demo/OtherDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.other_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtherDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
